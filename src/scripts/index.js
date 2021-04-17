2fde79f6dc7f9e279051cf814fbd287c18b64d3b
chris <clor09@gmail.com>
2021-04-17 17:09:20 -0500

    adding quick url shortener using local storage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,4 +4,46 @@ if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-console.log('webpack starterkit');
+class TestModel {
+  constructor() {
+
+    const baseUrl = window.location.origin;
+    const hashedUrl = window.location.hash;
+    
+    this._urlDataStore = window.localStorage;
+
+    this.shortenButton = document.querySelector('#shortenUrl-btn');
+    this.urlTextBox = document.querySelector('#rawUrlText');
+    this.shortenedUrlReadOnly = document.querySelector('#shortenedUrl');
+
+    if (hashedUrl !== '') {
+      this.returnShortenUrl(hashedUrl);
+    }
+
+    this.shortenButton.addEventListener('click', (event) => {
+      event.stopPropagation();
+      const safeUrl = encodeURIComponent(this.urlTextBox.value);
+
+      let shortUrlGenerated = this.generateShortUrl();
+      this._urlDataStore.setItem(shortUrlGenerated, safeUrl);
+
+      this.shortenedUrlReadOnly.value = `${baseUrl}${shortUrlGenerated}`;
+    });
+
+    window.addEventListener('hashchange', (event) => {
+      console.log(event);
+      this.returnShortenUrl(window.location.hash);
+    });
+  }
+
+  generateShortUrl(encodedRawUrlText) {
+      return '#' + Math.random().toString(16).substr(2, 8);
+  }
+
+  returnShortenUrl(hashedUrl) {
+    const retrievedShortenedUrl = this._urlDataStore.getItem(hashedUrl);
+    window.location.replace(decodeURIComponent(retrievedShortenedUrl));
+  }
+}
+
+const made = new TestModel();
